07bee723836b5201e7c1ffca266aff2c376d1881
Joy Ahmed <dev.joyahmed@gmail.com>
2023-11-30 06:25:33 +0600

    #

--- base_app/views.py
+++ base_app/views.py
@@ -4,29 +4,29 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 
 from base_app.models import CustomUser
-from .forms import UserLoginForm, UserRegistrationForm
+from .forms import UserLoginForm, CustomUserCreationForm
 
 
 def home(request):
     return render(request, 'home.html')
 
+
 def register_user(request):
     if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.set_password(form.cleaned_data['password'])
             user.generate_verification_token()
             user.email_verification()
             user.save()
-            messages.success(request, 'User registered successfully. Please check your email for verification.')
+            messages.success(
+                request, 'User registered successfully. Please check your email for verification.')
             return redirect('register_user')
     else:
-        form = UserRegistrationForm()
+        form = CustomUserCreationForm()
 
     return render(request, 'register.html', {'form': form})
-
-
 
 
 def verify_account(request):
@@ -38,9 +38,10 @@ def verify_account(request):
         if not user.is_active:
             user.is_active = True
             user.save()
-            messages.success(request, 'Account verified successfully. You can now log in.')
+            messages.success(
+                request, 'Account verified successfully. You can now log in.')
 
-             # Delete the verification token from the user instance
+            # Delete the verification token from the user instance
             user.verification_token = None
             user.save()
         else:
@@ -48,10 +49,7 @@ def verify_account(request):
     else:
         messages.error(request, 'Invalid verification token.')
 
-    return redirect('login') 
-
-
-
+    return redirect('login')
 
 
 def custom_login(request):
@@ -60,23 +58,19 @@ def custom_login(request):
         if form.is_valid():
             remember_me = form.cleaned_data.get('remember_me')
             if not remember_me:
-                request.session.set_expiry(0)  # Session expires when the browser is closed
+                # Session expires when the browser is closed
+                request.session.set_expiry(0)
 
             user = form.get_user()
             login(request, user)
-            return redirect('home')  # Change 'home' to the desired redirect after login
+            # Change 'home' to the desired redirect after login
+            return redirect('home')
     else:
         form = UserLoginForm()
 
     return render(request, 'login.html', {'form': form})
 
 
-
-
 def custom_logout(request):
     logout(request)
     return redirect('login')
-
-
-
-
